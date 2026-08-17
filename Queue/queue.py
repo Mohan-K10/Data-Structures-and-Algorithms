8ee9437a043b3ebe877799266399c8e95c944e99
Mohan-K10 <mohank18m@gmail.com>
2026-08-17 21:39:48 +0530

    Queue Data structure

--- Queue/queue.py
+++ Queue/queue.py
@@ -1,16 +1,21 @@
 class Queue:
+    # Queue is a data structure that follows FIFO (First In First Out).  
     def __init__(self):
         self.queue = []
-
+    # Checks if Queue is Empty or Not
     def isEmpty(self):
         return len(self.queue) == 0
-
+    
+    # Insert element in Queue
+    # It avoids front and rear to track values(which is used in c, cpp) but in python append automatically adds element at last
+    # Front and rear is used to check if the queue is underflow or overflow. 
     def insert(self, value):
         self.queue.append(value)
 
+    # Delete element in Queue
     def delete(self):
         if(self.isEmpty()):
-            raise Exception("The Queue is Empty")
+            print("The Queue is Empty")
         else:
             return self.queue.pop(0)
 
