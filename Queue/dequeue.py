02611911573cf661646358245a971b70d4f76bb7
Mohan-K10 <mohank18m@gmail.com>
2026-08-19 12:28:40 +0530

    Dequeue Data Structure

--- Queue/dequeue.py
+++ Queue/dequeue.py
@@ -1,22 +1,28 @@
 class Dequeue:
+    # dequeue(double ended queue) is data structure where the insertion and deletion happens on both ends of queue
     def __init__(self):
         self.items = []
 
+    # Checks if the queue is empty or not and returns boolean value
     def isEmpty(self):
         return len(self.items) == 0
 
+    # Inserts at end by using append function. By default append adds element at last
     def insertAtEnd(self, value):
         self.items.append(value)
 
+    # Deletes the last element by using pop(). By default pop deletes the last element
     def deleteAtEnd(self):
         if(self.isEmpty()):
             print("Dequeue is Empty")
         else:
             return self.items.pop()
 
+    # Inserts the element at the first position of the queue. By using insert(), place the element at 0th index.
     def insertAtStart(self, value):
         self.items.insert(0, value)
-
+        
+    # Deletes the element at the first position of the queue. By using insert(), place the element at 0th index.
     def deleteAtFront(self):
         if(self.isEmpty()):
             print("Dequeue is Empty")
